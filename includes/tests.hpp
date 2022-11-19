0dd3eb188718179d89b32d60c5b08f4e59091a23
Italo A <italoaurelior@gmail.com>
2022-11-19 08:50:38 -0300

    feat: add minimal tester

---
+++ includes/tests.hpp
@@ -0,0 +1,64 @@
+#ifndef TESTS_HPP
+# define TESTS_HPP
+
+# include <vector>
+# include <iostream>
+# include "./vector.hpp"
+
+
+# define RESET "\e[0m"
+# define GREEN "\e[92m"
+# define BLUE "\e[94m"
+# define BOLD "\e[1m"
+
+
+# define GOOD "✓"
+# define FAIL "❌"
+
+void	test_vector(void);
+
+inline void print_header(std::string str)
+{
+	int margin = (40 - str.length()) / 2;
+	int width = (margin * 2 + str.length()) + 2;
+	std::cout << BLUE << std::endl;
+	std::cout << std::string(width, '*') << std::endl;
+	std::cout << "*" << std::string(margin, ' ') << str << std::string(margin, ' ') << "*" << std::endl;
+	std::cout << std::string(width, '*') << std::endl;
+	std::cout << RESET;
+};
+
+template <typename T>
+inline void check(std::string name, T a, T b)
+{
+	std::string margin(38 - name.length(), ' ');
+	if (a == b)
+		std::cout << name << ": " << margin << BOLD << GREEN << GOOD << RESET << std::endl;
+	else
+		std::cout << name << ": " << margin << FAIL << std::endl;
+};
+
+inline void check(std::string name, bool good)
+{
+	std::string margin(38 - name.length(), ' ');
+	if (good)
+		std::cout << name << ": " << margin << BOLD << GREEN << GOOD << RESET << std::endl;
+	else
+		std::cout << name << ": " << margin << FAIL << std::endl;
+};
+
+template <typename T>
+bool operator==(ft::vector<T> &a, std::vector<T> &b)
+{
+	if (a.size() != b.size())
+		return (false);
+	if (a.empty() != b.empty())
+		return (false);
+	for (size_t i = 0; i < a.size(); i++)
+	{
+		if (a[i] != b[i])
+			return (false);
+	}
+	return (true);
+};
+#endif
