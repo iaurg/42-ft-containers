5b0e4f16c1f2db4c690b542f7ec18485c9d19b7d
Italo A <italoaurelior@gmail.com>
2022-11-22 22:40:25 -0300

    test: first functional tests

---
+++ tests/vector.cpp
@@ -0,0 +1,79 @@
+#include "../includes/tests.hpp"
+
+template <typename T>
+static void print_vector(T vec, std::string type)
+{
+	typename T::iterator it;
+
+	std::cout << BOLD << type << ", size: " << vec.size() << ", empty: " << vec.empty() << ", capacity: " << vec.capacity() << RESET << std::endl;
+	it = vec.begin();
+	while (it != vec.end())
+	{
+		std::cout << *it;
+		if (it + 1 != vec.end())
+			std::cout << " - ";
+		++it;
+	}
+	std::cout << std::endl;
+}
+
+template <typename T>
+static void print_vector_reverse(T vec, std::string type)
+{
+	typename T::iterator it;
+
+	std::cout << BOLD << type << ", size: " << vec.size() << ", empty: " << vec.empty() << ", capacity: " << vec.capacity() << RESET << std::endl;
+	it = vec.rbegin();
+	while (it != vec.rend())
+	{
+		std::cout << *it;
+		if (it + 1 != vec.rend())
+			std::cout << " - ";
+		++it;
+	}
+	std::cout << std::endl;
+}
+
+static void default_constructor(void)
+{
+	print_header("Default constructor");
+	ft::vector<int> v1;
+	std::vector<int> v2;
+	check("v1 == v2", v1 == v2);
+	v1.push_back(1);
+	v1.push_back(2);
+	v1.push_back(3);
+	v2.push_back(1);
+	v2.push_back(2);
+	v2.push_back(3);
+	check("v1 == v2", v1 == v2);
+}
+
+static void copy_constructor(void)
+{
+	print_header("Copy");
+	ft::vector<int> v1;
+	std::vector<int> v2;
+	v1.push_back(1);
+	v1.push_back(2);
+	v1.push_back(3);
+	v2.push_back(1);
+	v2.push_back(2);
+	v2.push_back(3);
+	ft::vector<int> v3(v1);
+	std::vector<int> v4(v2);
+	v1.push_back(42);
+	v2.push_back(42);
+	check("v1 == v2", (v1 == v2));
+	check("v3 == v4", (v3 == v4));
+	check("v2 != v4", (v2 != v4));
+	// check("v1 != v3", (v1 != v3));
+}
+
+void	test_vector(void)
+{
+	print_header("Vector");
+
+	default_constructor();
+	copy_constructor();
+}
