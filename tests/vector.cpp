2ccc3e6e8bd05439ca97b03d41e097a9c5a3b901
Italo A <italoaurelior@gmail.com>
2022-11-28 21:31:42 -0300

    feat: add more tests

--- tests/vector.cpp
+++ tests/vector.cpp
@@ -36,9 +36,75 @@ static void copy_constructor(void)
 	check("v1 != v3", (v1 != v3));
 }
 
-void	test_vector(void)
+static void max_size(void)
+{
+	print_header("Max size");
+	ft::vector<std::string> v1;
+	std::vector<std::string> v2;
+	check("v1.max_size() == v2.max_size()", v1.max_size(), v2.max_size());
+	v1.push_back("test");
+	v2.push_back("test");
+	check("v1.max_size() == v2.max_size()", v1.max_size(), v2.max_size());
+}
+
+static void resize(void)
+{
+	print_header("Resize");
+	ft::vector<std::string> v1;
+	std::vector<std::string> v2;
+	v1.resize(10, "test");
+	v2.resize(10, "test");
+	check("v1 == v2", (v1 == v2));
+	v1.resize(2, "42");
+	v2.resize(2, "42");
+	check("v1 == v2", (v1 == v2));
+}
+
+static void operators(void)
+{
+	print_header("Operators");
+	ft::vector<int> v1;
+	std::vector<int> v2;
+	ft::vector<int> v3;
+	std::vector<int> v4;
+	v1.push_back(1);
+	v1.push_back(2);
+	v1.push_back(3);
+	v2.push_back(1);
+	v2.push_back(2);
+	v2.push_back(3);
+	v3 = v1;
+	v4 = v2;
+	check("v1 == v2", (v1 == v3), (v2 == v4));
+	check("v1 != v2", (v1 != v3), (v2 != v4));
+	check("v1 > v2", (v1 > v3), (v2 > v4));
+	check("v1 < v2", (v1 < v3), (v2 < v4));
+	check("v1 >= v2", (v1 >= v3), (v2 >= v4));
+	check("v1 <= v2", (v1 <= v3), (v2 <= v4));
+	v1.push_back(42);
+	v2.push_back(42);
+	check("v1 == v2", (v1 == v3), (v2 == v4));
+	check("v1 != v2", (v1 != v3), (v2 != v4));
+	check("v1 > v2", (v1 > v3), (v2 > v4));
+	check("v1 < v2", (v1 < v3), (v2 < v4));
+	check("v1 >= v2", (v1 >= v3), (v2 >= v4));
+	check("v1 <= v2", (v1 <= v3), (v2 <= v4));
+	v3.push_back(43);
+	v4.push_back(43);
+	check("v1 == v2", (v1 == v3), (v2 == v4));
+	check("v1 != v2", (v1 != v3), (v2 != v4));
+	check("v1 > v2", (v1 > v3), (v2 > v4));
+	check("v1 < v2", (v1 < v3), (v2 < v4));
+	check("v1 >= v2", (v1 >= v3), (v2 >= v4));
+	check("v1 <= v2", (v1 <= v3), (v2 <= v4));
+}
+
+void test_vector(void)
 {
 	print_header("Vector");
 	default_constructor();
 	copy_constructor();
+	max_size();
+	resize();
+	operators();
 }
