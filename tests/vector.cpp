8b0205e24b3638266ac0a67c8afa408122e4453f
Italo A <italoaurelior@gmail.com>
2022-11-29 23:05:43 -0300

    feat: activate new tests

--- tests/vector.cpp
+++ tests/vector.cpp
@@ -60,6 +60,75 @@ static void resize(void)
 	check("v1 == v2", (v1 == v2));
 }
 
+static void access_operator(void)
+{
+	print_header("[] operator, at()");
+	ft::vector<int> v1;
+	std::vector<int> v2;
+	v1.push_back(1);
+	v1.push_back(2);
+	v1.push_back(3);
+	v2.push_back(1);
+	v2.push_back(2);
+	v2.push_back(3);
+	check("v1[0] == v2[0]", v1[0], v2[0]);
+	check("v1[1] == v2[1]", v1[1], v2[1]);
+	check("v1[2] == v2[2]", v1[2], v2[2]);
+	try
+	{
+		std::cout << "v1.at(100): " << v1.at(100) << ": " << FAIL << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << ": " << std::string(7, ' ') << BOLD << GREEN << GOOD << RESET << std::endl;
+	}
+	try
+	{
+		std::cout << "v2.at(100): " << v2.at(100) << ": " << FAIL << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << ": " << std::string(20, ' ') << BOLD << GREEN << GOOD << RESET << std::endl;
+	}
+	try
+	{
+		std::cout << "v1.at(-1): " << v1.at(-1) << ": " << FAIL << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << ": " << std::string(8, ' ') << BOLD << GREEN << GOOD << RESET << std::endl;
+	}
+	try
+	{
+		std::cout << "v2.at(-1): " << v2.at(-1) << ": " << FAIL << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what() << ": " << std::string(21, ' ') << BOLD << GREEN << GOOD << RESET << std::endl;
+	}
+}
+
+static void insert(void)
+{
+	print_header("Insert");
+	int test[] = {1, 2, 3};
+	ft::vector<int> v1;
+	std::vector<int> v2;
+	v1.insert(v1.begin(), 42);
+	v1.insert(v1.end(), 21);
+	v1.insert(v1.begin(), 10);
+	v1.insert(v1.begin() + 1, 11);
+	v1.insert(v1.begin() + 2, (size_t)3, 0);
+	v1.insert(v1.begin() + 1, test, test + 3);
+	v2.insert(v2.begin(), 42);
+	v2.insert(v2.end(), 21);
+	v2.insert(v2.begin(), 10);
+	v2.insert(v2.begin() + 1, 11);
+	v2.insert(v2.begin() + 2, (size_t)3, 0);
+	v2.insert(v2.begin() + 1, test, test + 3);
+	check("v1 == v2", v1 == v2);
+}
+
 static void operators(void)
 {
 	print_header("Operators");
@@ -99,6 +168,36 @@ static void operators(void)
 	check("v1 <= v2", (v1 <= v3), (v2 <= v4));
 }
 
+static void front_back(void)
+{
+	print_header("Front / Back");
+	ft::vector<int> v1;
+	std::vector<int> v2;
+	v1.push_back(1);
+	v1.push_back(2);
+	v1.push_back(3);
+	v2.push_back(1);
+	v2.push_back(2);
+	v2.push_back(3);
+	check("v1.front() == v2.front()", v1.front(), v2.front());
+	check("v1.back() == v2.back()", v1.front(), v2.front());
+}
+/*
+static void assign(void)
+{
+	print_header("Assign");
+	std::string test[] = {"Hey", "what's", "up", "?"};
+	ft::vector<std::string> v1;
+	std::vector<std::string> v2;
+	v1.assign(test, test + 4);
+	v2.assign(test, test + 4);
+	check("v1 == v2", v1 == v2);
+	v1.assign(10, "?");
+	v2.assign(10, "?");
+	check("v1 == v2", v1 == v2);
+}
+*/
+
 void test_vector(void)
 {
 	print_header("Vector");
@@ -106,5 +205,9 @@ void test_vector(void)
 	copy_constructor();
 	max_size();
 	resize();
+	access_operator();
+	insert();
 	operators();
+	front_back();
+	// assign();
 }
