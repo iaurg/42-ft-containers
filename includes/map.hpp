1e18b0f4efff9973e5cc9363cc22014a38972257
Italo A <italoaurelior@gmail.com>
2022-12-05 22:39:13 -0300

    feat: create all headers based on cplusplus.com

--- includes/map.hpp
+++ includes/map.hpp
@@ -135,6 +135,148 @@ namespace ft
          *
          */
         mapped_type &operator[](const key_type &k);
+
+        // MODIFIERS ===========================================================
+
+        /**
+         * @brief Inserts an element into the map container, if the container doesn't already contain an element with an equivalent key.
+         *
+         */
+        // TODO IMPLEMENT FT::PAIR
+        std::pair<iterator, bool> insert(const value_type &val);
+
+        /**
+         * @brief Inserts an element into the map container, if the container doesn't already contain an element with an equivalent key.
+         *
+         */
+        iterator insert(iterator position, const value_type &val);
+
+        /**
+         * @brief Inserts elements from range [first,last) into the map.
+         *
+         */
+        template <class InputIterator>
+        void insert(InputIterator first, InputIterator last);
+
+        /**
+         * @brief Erases an element from the map container.
+         *
+         */
+        void erase(iterator position);
+
+        /**
+         * @brief Erases an element from the map container.
+         *
+         */
+        size_type erase(const key_type &k);
+
+        /**
+         * @brief Erases elements from the map container.
+         *
+         */
+        void erase(iterator first, iterator last);
+
+        /**
+         * @brief Exchanges the content of the container by the content of x, which is another map of the same type. Sizes may differ.
+         *
+         */
+        void swap(map &x);
+
+        /**
+         * @brief Removes all elements from the map container (which are destroyed), leaving the container with a size of 0.
+         *
+         */
+        void clear();
+
+        // OBSERVERS ===========================================================
+
+        /**
+         * @brief Returns a copy of the comparison object used by the container to compare keys.
+         *
+         */
+        key_compare key_comp() const;
+
+        /**
+         * @brief  Returns a comparison object that can be used to compare two elements to get
+         * whether the key of the first one goes before the second.
+         *
+         */
+        value_compare value_comp() const;
+
+        // OPERATIONS ==========================================================
+
+        /**
+         * @brief Searches the container for an element with a key equivalent to k and returns
+         * an iterator to it if found, otherwise it returns an iterator to map::end.
+         *
+         */
+        iterator find(const key_type &k);
+
+        /**
+         * @brief Searches the container for an element with a key equivalent to k and returns
+         * an iterator to it if found, otherwise it returns an iterator to map::end.
+         *
+         */
+        const_iterator find(const key_type &k) const;
+
+        /**
+         * @brief Searches the container for elements with a key equivalent to k and returns
+         * the number of matches.
+         *
+         */
+        size_type count(const key_type &k) const;
+
+        /**
+         * @brief Returns an iterator pointing to the first element in the container whose key
+         * is not considered to go before k (i.e., either it is equivalent or goes after).
+         *
+         */
+        iterator lower_bound(const key_type &k);
+
+        /**
+         * @brief Returns an iterator pointing to the first element in the container whose key
+         * is not considered to go before k (i.e., either it is equivalent or goes after).
+         *
+         */
+        const_iterator lower_bound(const key_type &k) const;
+
+        /**
+         * @brief Returns an iterator pointing to the first element in the container whose key
+         * is considered to go after k.
+         *
+         */
+        iterator upper_bound(const key_type &k);
+
+        /**
+         * @brief Returns an iterator pointing to the first element in the container whose key
+         * is considered to go after k.
+         *
+         */
+        const_iterator upper_bound(const key_type &k) const;
+
+        /**
+         * @brief Returns the bounds of a range that includes all the elements in the container
+         * which have a key equivalent to k.
+         *
+         */
+        // TODO IMPLEMENT FT::PAIR
+        std::pair<const_iterator, const_iterator> equal_range(const key_type &k) const;
+
+        /**
+         * @brief Returns the bounds of a range that includes all the elements in the container
+         * which have a key equivalent to k.
+         *
+         */
+        // TODO IMPLEMENT FT::PAIR
+        std::pair<iterator, iterator> equal_range(const key_type &k);
+
+        // ALLOCATOR ===========================================================
+
+        /**
+         * @brief Returns a copy of the allocator object associated with the map.
+         *
+         */
+        allocator_type get_allocator() const;
     };
 }
 
